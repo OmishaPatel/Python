1d5cc191cf2b153c7ae7952527ca5dbda331dea0
Omisha Patel <64633546+OmishaPatel@users.noreply.github.com>
2021-07-02 23:56:02 -0400

    Add files via upload

---
+++ miscalgos/lowest_common_ancestor.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or p is root or q is root:
+            return root 
+        # left and right recursion
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        #one of the side is missing
+        return left or right
